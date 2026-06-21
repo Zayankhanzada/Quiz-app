7931a8fa2dce91649c8cc28d1039ad5fae07982e
Zayan Ahmed <zayankz456@gmail.com>
2026-06-22 02:49:53 +0500

    Quiz page done only sidebar resposive remaining

--- src/Quizstart.js
+++ src/Quizstart.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 // import logo from "./images/newlogo2.png";
 import { BrightnessHighFill, MoonFill } from "react-bootstrap-icons";
 import "./css/start.css";
 export default function Quizstart() {
-  // sidebar closing
+
+  const navigate = useNavigate();
+  // All states 
   const [Sidebar, setSidebar] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const options = [
+    "Option A",
+    "Option B",
+    "Option C",
+    "Option D",
+  ];
 
   // dark and light theme
   const [theme, settheme] = useState({
@@ -30,19 +40,29 @@ export default function Quizstart() {
       });
     }
   };
+  // Go back button
+  const handleConfirm = () => {
+    setShowModal(false);
+    navigate("/instructions");
+  };
+
 
   return (
     <div className="overflow-hidden h-100 " style={theme}>
       <nav
-        className="navbar navbar-expand border-bottom  text-center p-3 "
+        className="navbar navbar-expand border-bottom text-center p-3 "
         style={{ background: theme.backgroundColor }}
       >
         <div className="container-fluid justify-content-between align-items-center">
           <div className="d-flex align-items-center flex-row gap-3">
             <Link
               to="/instructions"
-              className="btn exit-button border-secondary"
+              className="btn border-secondary exit-button"
               style={{ color: theme.color }}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowModal(true);
+              }}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -58,40 +78,61 @@ export default function Quizstart() {
                 />
               </svg>
             </Link>
+            {showModal && (
+              <div
+                className="modal fade show d-block"
+                tabIndex="-1"
+                style={{ color: theme.color }}
+              >
+                <div className="modal-dialog modal-dialog-centered">
+                  <div className="modal-content rounded-3 shadow">
+                    <div className="modal-body p-4 text-center" style={{ color: theme.color }}>
+                      <h5 className="mb-0">Want to Exit Quiz?</h5>
+                      <p className="mb-0 mt-1">
+                        If you leave now, your current quiz progress will be lost and you'll need to start again.
+                      </p>
+                    </div>
+
+                    <div className="modal-footer flex-nowrap p-0">
+                      <button
+                        type="button"
+                        className="btn btn-lg exit-button btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0 border-end"
+                        onClick={handleConfirm}
+                        style={{ color: theme.color }}>
+                        <strong>Exit Quiz </strong>
+                      </button>
+
+                      <button
+                        type="button"
+                        className="btn btn-lg btn-success btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0"
+                        onClick={() => setShowModal(false)}
+                        style={{ color: theme.color }} >
+                        Continue! 👍
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
             <button
-              className="btn border-secondary fw-bold"
+              className="btn border-secondary sidebar-btn fw-bold d-inline-flex align-items-center gap-2"
               style={{ color: theme.color }}
               onClick={() => setSidebar(currentValue => !currentValue)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-box-arrow-right"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
-                />
-              </svg>
+              <span className="d-lg-flex d-none">Questions</span>
+              {Sidebar ? <i className="bi bi-box-arrow-left"></i> : <i className="bi bi-box-arrow-right"></i>}
             </button>
           </div>
-          <div className=" justify-content-center text-center text-white pt-1  ">
+          <div className=" justify-content-center align-items-center text-center text-white pt-1  ">
             <h2 className="text-center fs-4" style={{ color: theme.color }}>
               QuizBoard
             </h2>
 
           </div>
-          <div className="justify-content-end ">
+          <div className="justify-content-end align-items-center ">
             <button
-              className="btn btn-primary theme align-items-center fs-4"
-              onClick={() => changetheme()}>
+              className="btn theme align-items-center fs-4"
+              onClick={() => changetheme()} style={{ color: theme.color }}>
               {theme.mode === "dark"
                 ? <BrightnessHighFill />
                 : <MoonFill />}
@@ -101,18 +142,17 @@ export default function Quizstart() {
         </div>
       </nav>
 
-      <div className="row align-items-start gap-2   ">
+      <div className="row align-items-start gap-2 ">
         {/* Question offcanvas */}
         <div className={`col-12 ${Sidebar ? "col-lg-3 " : "col-lg-1"}`}>
-
-          <div className={`sidebar ${Sidebar ? "show-sidebar" : "hide-sidebar"} d-flex flex-column shadow-lg`}
+          <div className={`sidebar ${Sidebar ? "show-sidebar" : "hide-sidebar"} d-flex flex-column shadow-lg border-end`}
             style={theme}
           >
-            <div className="d-flex align-items-center justify-content-between p-3 border-bottom mt-1">
+            <div className="d-flex align-items-center justify-content-between p-3 border-bottom mt-1 ">
               <h5 className="fs-5 fw-semibold">Questions Menu:</h5>
 
             </div>
-            <div className="list-group list-group-flush border-bottom overflow-auto flex-grow-1">
+            <div className="list-group list-group-flush border-bottom overflow-auto flex-grow-1 custom-scrollbar">
               <a
                 href="/quiz"
                 className="list-group-item list-group-item-action  py-3 lh-sm"
@@ -293,37 +333,27 @@ export default function Quizstart() {
         {/* */}
         <div className={`col-12 ${Sidebar ? "col-lg-8 " : "col-lg-8 px-5"} p-4`}  >
           <div className="question-wrapper  my-5 ">
-            <p className="">Choose From Below Options</p>
+            <p className="">Choose (No of Opt) From Below Options</p>
             <h2 className="fs-4">
               <span className="fw-bolder fs-3">Q.</span> Here the Questions
               goes?
             </h2>
           </div>
-          <div className="options d-flex align-items-center justify-content-center  flex-column gap-3">
-            <button
-              className="btn w-100 p-3 text-start btn-outline-secondary"
-              style={theme}
-            >
-              Click here to select the Option
-            </button>
-            <button
-              className="btn w-100 p-3 text-start btn-outline-secondary"
-              style={theme}
-            >
-              Click here to select the Option
-            </button>
-            <button
-              className="btn w-100 p-3 text-start btn-outline-secondary"
-              style={theme}
-            >
-              Click here to select the Option
-            </button>
-            <button
-              className="btn w-100 p-3 text-start btn-outline-secondary"
-              style={theme}
-            >
-              Click here to select the Option
-            </button>
+          <div className="options d-flex align-items-center justify-content-center flex-column gap-3">
+            {options.map((option, index) => (
+              <div key={index} className="checkbox-wrapper mb-2">
+                <input
+                  type="checkbox"
+                  id={`option-${index}`}
+                  checked={selectedOption === index}
+                  onChange={() => setSelectedOption(index)}
+                />
+
+                <label htmlFor={`option-${index}`}>
+                  {option}
+                </label>
+              </div>
+            ))}
           </div>
           <div className=" mt-5">
             <button type="button" className="btn btn-secondary submit" style={theme}>
